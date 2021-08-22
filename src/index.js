6db346148f3c798ed0a8366ce50f687f0d13a11f
hannah-developer <hannahchoi.developer@gmail.com>
2021-08-22 22:26:23 +0100

    deleted unnecessary files

--- src/index.js
+++ src/index.js
@@ -3,14 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import TodoProvider from "./TodoProvider";
 
 ReactDOM.render(
-    <TodoProvider>
-        <React.StrictMode>
-            <App />
-        </React.StrictMode>
-    </TodoProvider>,
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
     document.getElementById("root")
 );
 
